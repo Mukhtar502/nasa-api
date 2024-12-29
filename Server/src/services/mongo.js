f4c87e8996a6647ff59178506c7c9062773a5f8b
Efunkunle <efunkunleolabode@gmail.com>
2024-12-29 23:58:41 +0100

    updated project

---
+++ Server/src/services/mongo.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+
+const MONGO_URL = process.env.MONGO_URL;
+
+
+//   "mongodb://127.0.0.1:27017/";
+//"mongodb+srv://nasa-api:DmB8J1E3zfRIX3aD@nasacluster.coxzp.mongodb.net/nasa?retryWrites=true&w=majority&appName=NASACluster";
+// ("mongodb+srv://nasa-api:<db_password>@nasacluster.coxzp.mongodb.net/?retryWrites=true&w=majority&appName=NASACluster");
+
+async function mongoConnect(){
+  await mongoose.connect(MONGO_URL)
+}
+async function mongoDisconnect() {
+  await mongoose.disconnect();
+}
+
+mongoose.connection.once("open", async () => {
+  console.log("MongoDB connection successful");
+});
+mongoose.connection.on("error", (err) => {
+  console.error(err);
+});
+
+module.exports = {
+  mongoConnect,
+  mongoDisconnect,
+};
