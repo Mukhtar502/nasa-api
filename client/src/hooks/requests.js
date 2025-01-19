c5584ddd2b97d3e35668a01b2c241d486dfa2b27
Efunkunle <efunkunleolabode@gmail.com>
2025-01-19 01:50:14 +0100

    added mongodb atlas for my database

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -1,4 +1,4 @@
-const API_URL = "http://www.localhost:8000/v1";
+const API_URL = "v1";
 async function httpGetPlanets() {
   // TODO: Once API is ready.
   const response = await fetch(`${API_URL}/planets`);
